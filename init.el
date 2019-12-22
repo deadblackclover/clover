9b97ac7b63a51aeb2ed3562df1e37e1939f68a3b
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-12-22 22:58:26 +0700

    Added keyboard.el

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (add-to-list 'load-path (expand-file-name "init" user-emacs-directory))
 (require 'window)
+(require 'keyboard)
 
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
---
+++ init/keyboard.el
@@ -0,0 +1,5 @@
+(global-set-key (kbd "C-o") 'find-file)
+(global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "<C-tab>") 'switch-to-buffer)
+
+(provide 'keyboard)
