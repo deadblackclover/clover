4c4f5545b470f1047a688f9808870e5b6c945773
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-12-25 22:17:47 +0700

    Update window.el

--- lisp/window.el
+++ lisp/window.el
@@ -1,4 +1,9 @@
-(setq frame-title-format (format "%s | Happy Hacking! | Clover Emacs v%s" (buffer-name) emacs-version))
+;;; init.el
+;;; Copyright (c) 2019, DEADBLACKCLOVER. This file is
+;;; licensed under the GNU General Public License version 3 or later. See
+;;; the LICENSE file.
+
+(setq frame-title-format (format "%s - Happy Hacking! - Clover Emacs v%s" (buffer-name) emacs-version))
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
