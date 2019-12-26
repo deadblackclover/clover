9adc43aba1372149062e2af6ba8ea6aeecff9190
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-12-26 10:27:46 +0700

    Update window.el

--- lisp/window.el
+++ lisp/window.el
@@ -1,4 +1,4 @@
-;;; init.el
+;;; window.el
 ;;; Copyright (c) 2019, DEADBLACKCLOVER. This file is
 ;;; licensed under the GNU General Public License version 3 or later. See
 ;;; the LICENSE file.
@@ -6,5 +6,8 @@
 (setq frame-title-format (format "%s - Happy Hacking! - Clover Emacs v%s" (buffer-name) emacs-version))
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(display-battery-mode 1) ;; battery status
+(global-linum-mode t) ;; global line mode
 
 (provide 'window)
