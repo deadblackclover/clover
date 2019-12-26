622fa9a8fa2d4d25ff96a84e6911e81922b55936
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-12-26 22:41:15 +0700

    Added install-packages.el

---
+++ install-packages.el
@@ -0,0 +1,29 @@
+;;; packages.el
+;;; Copyright (c) 2019, DEADBLACKCLOVER. This file is
+;;; licensed under the GNU General Public License version 3 or later. See
+;;; the LICENSE file.
+
+(require 'cl)
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(setq clover-packages-list
+      '(
+	cyberpunk-theme
+	clojure-mode
+	scala-mode
+	))
+
+(defun clover-auto-install-packages ()
+  (package-refresh-contents)
+  (mapc #'(lambda (package)
+	    (unless (package-installed-p package)
+	      (package-install package)))
+	clover-packages-list)
+  (save-buffers-kill-emacs))
+
+(clover-auto-install-packages)
