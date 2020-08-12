0911fb02b95a1bc19bb4cd5405f346c9babec410
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-08-12 22:29:22 +0700

    Added search in buffer

--- lisp/keyboard.el
+++ lisp/keyboard.el
@@ -4,15 +4,16 @@
 ;;; the LICENSE file.
 
 ;; File
-(global-set-key (kbd "C-o") 'find-file)
-(global-set-key (kbd "C-s") 'save-buffer)
-(global-set-key (kbd "C-w") 'kill-buffer)
+(global-set-key (kbd "C-o") 'find-file)       ;; Open file
+(global-set-key (kbd "C-s") 'save-buffer)     ;; Save file
+(global-set-key (kbd "C-w") 'kill-buffer)     ;; Close buffer
+(global-set-key (kbd "C-f") 'isearch-forward) ;; Search buffer
 
 ;; Copy Paste
 ;; (global-set-key (kbd "C-z") 'undo)
 ;; (global-set-key (kbd "C-c") 'kill-ring-save) ;; Copy
-;; (global-set-key (kbd "C-x") 'kill-region) ;; Cut
-;; (global-set-key (kbd "C-v") 'yank) ;; Paste
+;; (global-set-key (kbd "C-x") 'kill-region)    ;; Cut
+;; (global-set-key (kbd "C-v") 'yank)           ;; Paste
 (cua-mode 1)
 
 ;; Select Text
