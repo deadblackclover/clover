990edf8ffb3393b1b329cfb10a87f31cd1239129
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-08-16 21:43:42 +0700

    Added create untitled buffer for c/c++

--- lisp/c-setting.el
+++ lisp/c-setting.el
@@ -6,6 +6,12 @@
 (require 'disaster)
 
 ;; C
+(defun clover/new-c () 
+  (interactive) 
+  (switch-to-buffer (get-buffer-create "untitled.c")) 
+  (setq initial-major-mode (quote c-mode)) 
+  (funcall initial-major-mode))
+
 (defun clover/c-mode-hook () 
   (setq c-set-style "k&r") 
   (setq c-basic-offset 4))
@@ -27,16 +33,22 @@
 			 (define-key c-mode-map (kbd "<f5>") 'compile)))
 
 ;; C++
+(defun clover/new-c++ () 
+  (interactive) 
+  (switch-to-buffer (get-buffer-create "untitled.cpp")) 
+  (setq initial-major-mode (quote c++-mode)) 
+  (funcall initial-major-mode))
+
 (defun clover/c++-mode-hook () 
   (setq c-basic-offset 4))
 (add-hook 'c++-mode-hook 'clover/c++-mode-hook)
 
 ;; Compile command
 (add-hook 'c++-mode-hook (lambda () 
-			 (set (make-local-variable 'compile-command) 
-			      (format "g++ -o %s %s && ./%s" (file-name-base buffer-file-name) 
-				      (buffer-file-name) 
-				      (file-name-base buffer-file-name)))))
+			   (set (make-local-variable 'compile-command) 
+				(format "g++ -o %s %s && ./%s" (file-name-base buffer-file-name) 
+					(buffer-file-name) 
+					(file-name-base buffer-file-name)))))
 
 ;; Compile and run
 (add-hook 'c++-mode-hook (lambda () 
