a812849501b6b86b2f05d29f2dc4b15ae26e8ed6
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-08-18 22:52:13 +0700

    Change compile for c/c++

--- lisp/c-setting.el
+++ lisp/c-setting.el
@@ -17,20 +17,19 @@
   (setq c-basic-offset 4))
 (add-hook 'c-mode-hook 'clover/c-mode-hook)
 
-;; Compile command
-(add-hook 'c-mode-hook (lambda () 
-			 (set (make-local-variable 'compile-command) 
-			      (format "gcc -o %s %s && ./%s" (file-name-base buffer-file-name) 
-				      (buffer-file-name) 
-				      (file-name-base buffer-file-name)))))
-
 ;; Disassemble
 (add-hook 'c-mode-hook (lambda () 
 			 (define-key c-mode-map (kbd "M-d") 'disaster)))
 
+(defun clover/c-compile () 
+  (interactive) 
+  (compile (format "gcc -o %s %s && ./%s" (file-name-base buffer-file-name) 
+		   (buffer-file-name) 
+		   (file-name-base buffer-file-name))))
+
 ;; Compile and run
 (add-hook 'c-mode-hook (lambda () 
-			 (define-key c-mode-map (kbd "<f5>") 'compile)))
+			 (define-key c-mode-map (kbd "<f5>") 'clover/c-compile)))
 
 ;; C++
 (defun clover/new-c++ () 
@@ -43,19 +42,19 @@
   (setq c-basic-offset 4))
 (add-hook 'c++-mode-hook 'clover/c++-mode-hook)
 
-;; Compile command
-(add-hook 'c++-mode-hook (lambda () 
-			   (set (make-local-variable 'compile-command) 
-				(format "g++ -o %s %s && ./%s" (file-name-base buffer-file-name) 
-					(buffer-file-name) 
-					(file-name-base buffer-file-name)))))
-
 ;; Disassemble
 (add-hook 'c++-mode-hook (lambda () 
 			   (define-key c++-mode-map (kbd "M-d") 'disaster)))
 
+(defun clover/c++-compile () 
+  (interactive) 
+  (compile (format "g++ -o %s %s && ./%s" (file-name-base buffer-file-name) 
+		   (buffer-file-name) 
+		   (file-name-base buffer-file-name))))
+
+
 ;; Compile and run
 (add-hook 'c++-mode-hook (lambda () 
-			   (define-key c++-mode-map (kbd "<f5>") 'compile)))
+			   (define-key c++-mode-map (kbd "<f5>") 'clover/c++-compile)))
 
 (provide 'c-setting)
