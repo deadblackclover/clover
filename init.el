fcfd35a566822487491cb60db92e0a3bf44f5e07
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-09-01 21:30:56 +0700

    Bump version

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (package-initialize)
 
 ;; Global Variables
-(setq clover-version "v0.1.2")
+(setq clover-version "v0.2.0")
 (setq clover-path (file-name-directory (or load-file-name buffer-file-name)))
 
 ;; Custom file
