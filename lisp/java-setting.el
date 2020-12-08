bd45c92ed63b6e4ea86a5d8a46fe916551e96cc0
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-12-08 22:14:16 +0700

    Fix

--- lisp/java-setting.el
+++ lisp/java-setting.el
@@ -7,20 +7,23 @@
 
 ;;; Code:
 (require 'gradle-mode)
-(add-hook 'java-mode-hook '(lambda() (gradle-mode 1)))
+(add-hook 'java-mode-hook '(lambda() 
+                             (gradle-mode 1)))
 
-(defun clover-new-java ()
-  "Create a Java file."
-  (interactive)
-  (switch-to-buffer (get-buffer-create "untitled.java"))
-  (setq initial-major-mode (quote java-mode))
+(defun clover-new-java () 
+  "Create a Java file." 
+  (interactive) 
+  (switch-to-buffer (get-buffer-create "untitled.java")) 
+  (setq initial-major-mode (quote java-mode)) 
   (funcall initial-major-mode))
 
-(defun build-and-run ()
-  (interactive)
+(defun clover-build-and-run-java () 
+  (interactive) 
   (gradle-run "build run"))
 
-(define-key gradle-mode-map (kbd "C-c C-r") 'build-and-run)
+;; Run
+(add-hook 'java-mode-hook (lambda () 
+                            (define-key java-mode-map (kbd "<f5>") 'clover-build-and-run-java)))
 
 (provide 'java-setting)
 ;;; java-setting.el ends here
