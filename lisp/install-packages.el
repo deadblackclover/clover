c751d68bf7c6bd4ef10036f2200890e1129a7c6c
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-12-17 23:36:34 +0700

    Added ag

--- lisp/install-packages.el
+++ lisp/install-packages.el
@@ -14,7 +14,7 @@
 
 (setq package-enable-at-startup nil)
 
-(setq clover-packages-list '(c-eldoc cargo company company-solidity company-lsp cyberpunk-theme diff-hl
+(setq clover-packages-list '(c-eldoc ag cargo company company-solidity company-lsp cyberpunk-theme diff-hl
                                      disaster elisp-format elpher epresent flycheck gradle-mode hl-todo js2-mode
                                      lsp-metals lsp-mode lsp-ui magit mastodon neotree nyan-mode package-lint
                                      paredit php-mode plan9-theme posframe py-autopep8 python-mode racket-mode
