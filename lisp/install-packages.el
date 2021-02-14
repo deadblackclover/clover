381fec056561d2990689d3149a9eaea6fdea8777
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-02-14 09:11:45 +0700

    Added pacmacs

--- lisp/install-packages.el
+++ lisp/install-packages.el
@@ -17,9 +17,9 @@
 (setq clover-packages-list '(c-eldoc ag cargo company company-solidity company-lsp cyberpunk-theme diff-hl
                                      disaster elisp-format elpher epresent flycheck gradle-mode hl-todo js2-mode
                                      lolcat lsp-metals lsp-mode lsp-ui magit mastodon nasm-mode neotree nyan-mode
-                                     package-lint paredit php-mode plan9-theme posframe py-autopep8 python-mode
-                                     racket-mode rainbow-delimiters reverse-im rust-mode sbt-mode scala-mode
-                                     solidity-mode twtxt))
+                                     package-lint pacmacs paredit php-mode plan9-theme posframe py-autopep8
+                                     python-mode racket-mode rainbow-delimiters reverse-im rust-mode sbt-mode
+                                     scala-mode solidity-mode twtxt))
 
 (defun clover-install-packages ()
   "Auto install packages."
