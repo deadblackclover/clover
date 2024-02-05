e720c17a5abdb25e57c7b0c0ae9da7b375d23b32
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2024-02-05 18:23:27 +0700

    Added multiple-cursors and tuareg

--- lisp/install-packages.el
+++ lisp/install-packages.el
@@ -19,11 +19,11 @@
                                      disaster dot-ticker ef-themes elisp-format elm-mode elpher
                                      epresent fasm-mode find-file-in-repository flycheck gradle-mode
                                      haskell-mode hl-todo js2-mode kotlin-mode lolcat lsp-metals
-                                     lsp-mode lsp-ui magit mastodon nasm-mode nyan-mode package-lint
-                                     pacmacs paredit php-mode plan9-theme posframe py-autopep8
-                                     python-mode racket-mode rainbow-delimiters reddigg reverse-im
-                                     rust-mode sbt-mode scala-mode solidity-mode treemacs twtxt
-                                     wat-mode xah-find xmr-ticker zig-mode))
+                                     lsp-mode lsp-ui magit mastodon multiple-cursors nasm-mode
+                                     nyan-mode package-lint pacmacs paredit php-mode plan9-theme
+                                     posframe py-autopep8 python-mode racket-mode rainbow-delimiters
+                                     reddigg reverse-im rust-mode sbt-mode scala-mode solidity-mode
+                                     treemacs tuareg twtxt wat-mode xah-find xmr-ticker zig-mode))
 
 (defun clover-install-packages ()
   "Auto install packages."
